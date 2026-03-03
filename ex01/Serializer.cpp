861b5850769e10e72d56d194140bc7d4fff789b1
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-03-03 20:24:44 +0100

    docs: add detailed documentation for Serializer class methods

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -1,19 +1,47 @@
 #include "Serializer.hpp"
 
+/**
+ * @brief Default constructor.
+ */
 Serializer::Serializer() {}
+
+/**
+ * @brief Copy constructor.
+ * @param other - source instance.
+ */
 Serializer::Serializer(const Serializer &other) { (void)other; }
+
+/**
+ * @brief Destructor.
+ */
 Serializer::~Serializer() {}
+
+/**
+ * @brief Copy assignment operator.
+ * @param other - source instance.
+ * @return assigned instance.
+ */
 Serializer &Serializer::operator=(const Serializer &other)
 {
 	(void)other;
 	return (*this);
 }
 
+/**
+ * @brief Converts Data pointer to raw integer value.
+ * @param ptr - pointer to serialize.
+ * @return serialized uintptr_t value.
+ */
 uintptr_t Serializer::serialize(Data *ptr)
 {
 	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
+/**
+ * @brief Converts raw integer value back to Data pointer.
+ * @param raw - raw integer representation.
+ * @return deserialized Data pointer.
+ */
 Data *Serializer::deserialize(uintptr_t raw)
 {
 	return (reinterpret_cast<Data *>(raw));
